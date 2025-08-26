98769d6440bbe5079d365249206987467b357474
Hachim <80651380+sfsugrad@users.noreply.github.com>
2025-08-26 11:54:05 -0500

    refactor: update DTFMTH for Python 3.12

--- DTFMTH.py
+++ DTFMTH.py
@@ -1,95 +1,51 @@
 import sys
-
-import pyodbc
-
 import argparse
-
 import re
-
-import codecs
-
-import string
-
-import os
-
 from datetime import datetime, timedelta
+from pathlib import Path
 
-from tzlocal import get_localzone  # pip install tzlocal
 
 # folders = ['DTFMTA','DTFMTD','DTFMTE','DTFMTH','DTFMTI','DTFMTM','DTFPDQ']
-
 folders = ['DTFMTH']
 
 DAY = timedelta(1)
 
-infile = ''
-
-out_file = ''
-
 office = []
-
 officecode = []
-
 lines = []
-
 files = []
-
 patterns = []
-
 first_row = []
 
-DAY = timedelta(1)
-
-local_tz = get_localzone()  # get local timezone
-
-now = datetime.now(local_tz)  # get timezone-aware datetime object
-
-day_ago = local_tz.normalize(now - DAY)  # exactly 24 hours ago, time may differ
-
-naive = now.replace(tzinfo=None) - DAY  # same time
-
-yesterday = local_tz.localize(naive, is_dst=None)  # but elapsed hours may differ
-
-yesterday_date = yesterday.strftime("%m%d")
-
-yesterday_date_full = yesterday.strftime("%Y%m%d")
-
-today = now.strftime("%Y%m%d")
+# Date placeholders; populated during runtime
+yesterday_date = ""
+yesterday_date_full = ""
+today = ""
 
 
-def parse_stuff(infile, folder, outfile):
-    # outfile = open(r'\\d1wrptfsrprd3\reports\Firm10\MTGO\\' + yesterday_date_full + '\\' + folder + '.txt','w')
+def parse_stuff(infile, folder: str, outdir: str) -> None:
+    """Filter lines from ``infile`` using ``patterns`` and write them to ``outdir``.
 
-    # print(outfile + 'MTGO\\' + today)
+    Parameters
+    ----------
+    infile : io.TextIOBase
+        Input file to scan.
+    folder : str
+        Name of the folder being processed.
+    outdir : str
+        Root directory for output files.
+    """
 
-    if not os.path.exists(outfile + 'MTGO\\' + today):
-        os.mkdir(outfile + 'MTGO\\' + today)
+    output_dir = Path(outdir) / "MTGO" / today
+    output_dir.mkdir(parents=True, exist_ok=True)
+    out_path = output_dir / f"{folder}.txt"
 
-    out_file = open(outfile + 'MTGO\\' + today + '\\' + folder + '.txt', 'w')
-
-    # r'\\d1wrptfsrprd3\reports\Firm10\MTGO\
-
-    aline = infile.readline()
-
-    while aline:
-
-        for pattern in patterns:
-
-            m = re.search(pattern, aline)
-
-            if m:
-
-                out_file.write(aline)
-
-                aline = infile.readline()
-
-            else:
-
-                aline = infile.readline()
-
-    infile.close()
-
-    out_file.close()
+    with out_path.open("w", encoding="utf-8") as out_file:
+        for line in infile:
+            for pattern in patterns:
+                if re.search(pattern, line):
+                    out_file.write(line)
+                    break
 
 
 def main(argv=None):
@@ -97,85 +53,65 @@ def main(argv=None):
         argv = sys.argv
 
     arg_parser = argparse.ArgumentParser()
-
     arg_parser.add_argument("SourceFilePath")
-
     arg_parser.add_argument("Outfile")
-
     arg_parser.add_argument("Server")
-
     arg_parser.add_argument("Database")
-
     args = arg_parser.parse_args()
 
+    # Import dependencies lazily so that help text can be displayed without them
+    from tzlocal import get_localzone  # pip install tzlocal
+    import pyodbc
+
     filepath = args.SourceFilePath
-
-    # print(filepath)
-
     out_file = args.Outfile
-
-    # print(out_file)
-
-    # dbserver = 'D1WLUOSQLNP1\dev'
-
-    # dbserver = 'APOLLO\HISTORY'
-
     dbserver = args.Server
-
-    # db = 'Process10'
-
-    # db = 'Worldwide'
-
     db = args.Database
 
-    con_str = 'DRIVER={SQL Server Native Client 10.0};' + 'SERVER={};DATABASE={};Trusted_Connection=yes'.format(
-        dbserver, db)
+    # Compute date values
+    global today, yesterday_date, yesterday_date_full
+    local_tz = get_localzone()  # get local timezone
+    now = datetime.now(local_tz)  # get timezone-aware datetime object
+    day_ago = local_tz.normalize(now - DAY)  # exactly 24 hours ago, time may differ
+    naive = now.replace(tzinfo=None) - DAY  # same time
+    yesterday = local_tz.localize(naive, is_dst=None)  # but elapsed hours may differ
+    yesterday_date = yesterday.strftime("%m%d")
+    yesterday_date_full = yesterday.strftime("%Y%m%d")
+    today = now.strftime("%Y%m%d")
+
+    con_str = (
+        "DRIVER={SQL Server Native Client 10.0};"
+        "SERVER={};DATABASE={};Trusted_Connection=yes".format(dbserver, db)
+    )
 
     cnxn = pyodbc.connect(con_str)
-
     cursor = cnxn.cursor()
-
-    select_officecode = "select distinct officecode from correspondentoffice where correspondentcode = 'MTGO' and len(officecode) = 3"
-
+    select_officecode = (
+        "select distinct officecode from correspondentoffice "
+        "where correspondentcode = 'MTGO' and len(officecode) = 3"
+    )
     cursor.execute(select_officecode)
-
     first_row = cursor.fetchall()
-
     for x in first_row:
         office.append(x)
-
     for i in office:
-        s = '|'.join(i)
-
+        s = "|".join(i)
         patterns.append(s)
-
     for folder in folders:
-
-        # print(folder)
-
         try:
-
-            # infile = open(r'\\d1wappsvrprd1\apps\DTC\DTFPART' + '\\' + folder + '\\' + folder + '_OUTPUT_' + yesterday_date + '.txt','rb')
-
-            # infile = open(r'\\d1wrptfsrnp1\dev\APPS\DTC\DTFPART' + '\\' + folder + '\\' + folder + '_OUTPUT_' + yesterday_date + '.txt','rb')
-
-            infile = open(
-                filepath + 'DTC\DTFPART' + '\\' + folder + '\\' + folder + '_OUTPUT_' + yesterday_date + '.txt', 'rb')
-
-            # print(infile)
-
-            parse_stuff(infile, folder, out_file)
-
-        except IOError as (errno, strerror):
-
-            print
-            "I/O error({0}): {1}".format(errno, strerror)
+            infile_path = (
+                Path(filepath)
+                / "DTC"
+                / "DTFPART"
+                / folder
+                / f"{folder}_OUTPUT_{yesterday_date}.txt"
+            )
+            with infile_path.open("r", encoding="utf-8") as infile:
+                parse_stuff(infile, folder, out_file)
+        except OSError as e:
+            print(f"I/O error({e.errno}): {e.strerror}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-# infile.close()
-
-# outfile.close()
 
