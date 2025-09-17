357d8536a3d8aca4d3826240b1e9de98551620be
Hachim <80651380+sfsugrad@users.noreply.github.com>
2025-09-17 15:21:47 -0500

    revert changes

--- sql_console/setup.py
+++ sql_console/setup.py
@@ -1,12 +1,22 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
     name='sql_console',
-    version='0.1',
-    packages=find_packages(),
+    version='0.3.1',
+    author='Rory Linehan',
+    author_email='rlinehan@apexclearing.com',
+    packages=find_packages(include=['sql_console']),
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://github.apexclearing.local/rlinehan/sql_console',
+    # license='LICENSE.txt',
+    description='Package for connecting seamlessly to SQL systems at Apex',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Ensure rendering of Markdown content
     install_requires=[
-        'pymysql',
-        'psycopg2',
-        'sqlalchemy',
+        "pyodbc==4.0.16",
+        "pymssql",
+        "pymysql",
+        # "mysql.connector",  # Uncomment if needed
+        "psycopg2",
     ],
 )
